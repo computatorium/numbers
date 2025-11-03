b7b6e1ee1652892a65a7ecc5810a27ae6416374b
malcjones <33630760+malcjones@users.noreply.github.com>
2025-11-03 13:39:21 -0600

    added a main for posterity

--- totient.py
+++ totient.py
@@ -35,3 +35,8 @@ def phi(n: int) -> int:
 # a smaller, much slower GCD-dependent version to show the basic
 def tiny_phi(n: int) -> int:
     return sum(1 for f in range(1, n + 1) if gcd(n, f) == 1)
+
+# test the functions
+if __name__ == "__main__":
+    for i in range(1, 21):
+        print(f"{i} - big({phi(i)}), tiny({tiny_phi(i)})")
